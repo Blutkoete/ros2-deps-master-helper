890fd5f50a1b8f0deefcf565f36ed9e3da5057a4
Tobias Borgert <tobias.borgert@gmail.com>
2020-05-20 18:48:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ros2-deps-master-helper
---
+++ ros2-deps-master-helper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+PYQT5_PATH = '/usr/lib/PyQt5/'
+SIP_PATH = '/usr/share/sip/'
+BINDINGS_PATH = '/usr/lib/PyQt5/'
+
+IGNORE_LIST = ['__pycache__', 'bindings']
+
+if __name__ == '__main__':
+    if os.path.isdir('')
